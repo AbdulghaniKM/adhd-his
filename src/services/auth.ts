3ba5f5cc84dcaf7672dc66dc3031d1bd97dd75c0
AbdulghaniKM <abdulghanim70@gmail.com>
2026-04-09 22:57:29 +0300

    Refactor ADHDx project configuration; update API paths and remove unused services.

---
+++ src/services/auth.ts
@@ -0,0 +1,23 @@
+import { API_PATHS } from '../config/api-paths';
+import api from '../plugins/axios';
+
+const authService = {
+  adminLogin: async (username: string, password: string) => {
+    const response = await api.post(API_PATHS.AUTH.ADMIN_LOGIN, { username, password });
+    return response.data;
+  },
+  doctorLogin: async (username: string, password: string) => {
+    const response = await api.post(API_PATHS.AUTH.DOCTOR_LOGIN, { username, password });
+    return response.data;
+  },
+  labtechLogin: async (username: string, password: string) => {
+    const response = await api.post(API_PATHS.AUTH.LABTECH_LOGIN, { username, password });
+    return response.data;
+  },
+  logout: async () => {
+    const response = await api.post(API_PATHS.AUTH.LOGOUT);
+    return response.data;
+  },
+};
+
+export default authService;
